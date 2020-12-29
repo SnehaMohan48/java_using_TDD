57edcf7083229f7f8c36808c24450fb5ee6a70c8
Sneha Mohan <snehamohan0408@gmail.com>
2020-12-29 15:45:45 +0530

    adding function to get the input sum

--- java_Using_TDD/src/java_Using_TDD/stringCalculator.java
+++ java_Using_TDD/src/java_Using_TDD/stringCalculator.java
@@ -4,15 +4,30 @@ public class stringCalculator
 {
 public int addString(String input)
 {
+	String[] numbers = input.split(",");
 	if(input.isEmpty())
 	{
 		return 0;
 	}
 	else if(input.length()==1)
+	{
 	return Integer.parseInt(input);
-	return Integer.parseInt(input);
-		
+	}
+	else if(numbers.length>1)
+	{
+		return result(numbers);
+	}
+	return Integer.parseInt(input);		
+}
+private int result(String[] numbers)
+{
+	int result=0;
+	for(String currentNumber:numbers)
+	{
+		result += Integer.parseInt(currentNumber);
+	}
+	return result;
+}
 }
 
-}
 
